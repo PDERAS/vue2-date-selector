6e816cef2ec8e33e758a32cb85744b9f395baaef
Cody Moorhouse <cody.moorhouse@pderas.com>
2018-03-14 12:05:48 -0600

    Change how the component is exported

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import DateSelector from './components/DateSelector.vue'
 
-export default {
-    install (Vue, options) {
-        Vue.component('date-selector', DateSelector);
-    }
+function install(Vue, options) {
+    Vue.component('date-selector', DateSelector);
 }
+
+export default install;
